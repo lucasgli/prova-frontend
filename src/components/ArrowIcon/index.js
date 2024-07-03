d6ba8b9d0bfb20b4e7a86c1fc26328df0a32221e
Lucas Gabriel <eslucasgabriel@gmail.com>
2024-07-03 11:41:25 -0400

    style(arrow-icon): update icon and animation on click

--- src/components/ArrowIcon/index.js
+++ src/components/ArrowIcon/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles.css";
 export default function ArrowIcon({ isOpen, isAnimating }) {
@@ -9,7 +9,7 @@ export default function ArrowIcon({ isOpen, isAnimating }) {
       style={{
         color: isOpen ? "blue" : "",
       }}
-      icon={isOpen ? faChevronUp : faChevronDown}
+      icon={faChevronDown}
       className={isAnimating ? (isOpen ? "rotate-up" : "rotate-down") : null}
     />
   );
