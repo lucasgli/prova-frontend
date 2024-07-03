904d28ca00b3118155b994c8e06ffd8e891752fb
Lucas Gabriel <eslucasgabriel@gmail.com>
2024-07-03 13:18:10 -0400

    style(arrow-icon): update animation style

--- src/components/ArrowIcon/index.js
+++ src/components/ArrowIcon/index.js
@@ -3,11 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles.css";
+
 export default function ArrowIcon({ isOpen, isAnimating }) {
   return (
     <FontAwesomeIcon
       style={{
         color: isOpen ? "blue" : "",
+        transform: isOpen ? "rotate(180deg)" : "rotate(0deg)",
       }}
       icon={faChevronDown}
       className={isAnimating ? (isOpen ? "rotate-up" : "rotate-down") : null}
